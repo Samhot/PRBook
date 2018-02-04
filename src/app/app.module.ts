418f178c94aa276dd585d4250d499b7e7c96e7cd
Thomas Punzano <thomas.punzano@atos.net>
2018-02-04 22:15:04 +0100

    material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router/src/router_module';
 import { RoutesRecognized } from '@angular/router/src/events';
+import { MaterialModule } from './material.modules';
 
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
-
-// import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-// import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -25,13 +23,6 @@ import { TodosComponent } from './todos/todos.component';
 import { TodoService } from './todo.service';
 import { TodosDetailComponent } from './todos-detail/todos-detail.component';
 
-// import { TodoListComponent } from './todo-list/todo-list.component';
-// import { TodoListFooterComponent } from './todo-list-footer/todo-list-footer.component';
-// import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
-// import { TodoDataService } from './todo-data.service';
-// import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
-// import { ApiService } from './api.service';
-
 
 @NgModule({
   declarations: [
@@ -43,17 +34,14 @@ import { TodosDetailComponent } from './todos-detail/todos-detail.component';
     WodSearchComponent,
     TodosComponent,
     TodosDetailComponent,
-    // TodoListComponent,
-    // TodoListFooterComponent,
-    // TodoListHeaderComponent,
-    // TodoListItemComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     HttpModule,
-    HttpClientModule
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [
     WodService,
