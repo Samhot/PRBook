85c1fb79117aab32258a4b54173a557fcc6dc7b6
THOMAS PUNZANO <thomas.punzano@atos.net>
2018-06-25 16:40:59 +0200

    chips ok but no filter on autocomplete

--- src/app/dialogs/add/add.dialog.component.ts
+++ src/app/dialogs/add/add.dialog.component.ts
@@ -1,11 +1,11 @@
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Component, ElementRef, Inject, ViewChild } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatAutocompleteSelectedEvent, MatChipInputEvent, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { DataService } from '../../services/data.service';
+import { MouvService } from '../../services/mouv.service';
 import { Mouvement } from '../../_models/mouvements';
 import { Wod } from '../../_models/wod';
 
@@ -15,7 +15,7 @@ import { Wod } from '../../_models/wod';
   styleUrls: ['../../dialogs/add/add.dialog.css']
 })
 
-export class AddDialogComponent {
+export class AddDialogComponent implements OnInit {
 
   visible = true;
   selectable = true;
@@ -24,57 +24,48 @@ export class AddDialogComponent {
 
   separatorKeysCodes = [ENTER, COMMA];
 
-  fruitCtrl = new FormControl();
+  mouvCtrl = new FormControl();
 
-  filteredFruits: Observable<any[]>;
+  filteredMouvs$: Observable<Mouvement[]>;
 
-  fruits = [
-  ];
+  mouvs = [];
 
-  allFruits = [
-    'Apple',
-    'Lemon',
-    'Lime',
-    'Orange',
-    'Strawberry'
-  ];
-
-  private API_URL = 'http://localhost:5000';
-  dataChange: BehaviorSubject<Mouvement[]> = new BehaviorSubject<Mouvement[]>([]);
-
-  @ViewChild('fruitInput') fruitInput: ElementRef;
+  allMouvs = [];
 
   formControl = new FormControl('', [
     Validators.required
     // Validators.email,
   ]);
 
+  @ViewChild('mouvInput') mouvInput: ElementRef;
+
   constructor(public dialogRef: MatDialogRef<AddDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: Wod,
               public dataService: DataService,
-              private httpClient: HttpClient) {
-                this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
-                  startWith(null),
-                  map((fruit: string | null) => fruit ? this.filter(fruit) : this.allFruits.slice()));
+              private mouvService: MouvService) {
+
               }
 
-  getAllWods(): void {
-    this.httpClient.get<Wod[]>(this.API_URL + '/mouvs')
-    .subscribe(data => {
-        this.dataChange.next(data);
-      },
-      (error: HttpErrorResponse) => {
-      console.log (error.name + ' ' + error.message);
-      });
+  ngOnInit() {
+    this.getMouvs();
+    this.filteredMouvs$ = this.mouvCtrl.valueChanges.pipe(
+      startWith(null),
+      map((mouv: string | null) => mouv ? this.filter(mouv) : this.allMouvs.slice()));
+  }
+
+
+  getMouvs(): void {
+    this.mouvService.getMouvs()
+        .subscribe(mouvs => this.allMouvs = mouvs);
   }
 
   add(event: MatChipInputEvent): void {
     const input = event.input;
     const value = event.value;
 
-    // Add our fruit
+    // Add our mouv
     if ((value || '').trim()) {
-      this.fruits.push(value.trim());
+      this.mouvs.push(value.trim());
     }
 
     // Reset the input value
@@ -82,26 +73,26 @@ export class AddDialogComponent {
       input.value = '';
     }
 
-    this.fruitCtrl.setValue(null);
+    this.mouvCtrl.setValue(null);
   }
 
-  remove(fruit: any): void {
-    const index = this.fruits.indexOf(fruit);
+  remove(mouv: any): void {
+    const index = this.mouvs.indexOf(mouv);
 
     if (index >= 0) {
-      this.fruits.splice(index, 1);
+      this.mouvs.splice(index, 1);
     }
   }
 
   filter(name: string) {
-    return this.allFruits.filter(fruit =>
-        fruit.toLowerCase().indexOf(name.toLowerCase()) === 0);
+    return this.allMouvs.filter(mouv =>
+        mouv.name.toLowerCase().indexOf(name.toLowerCase()) === 0);
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.fruits.push(event.option.viewValue);
-    this.fruitInput.nativeElement.value = '';
-    this.fruitCtrl.setValue(null);
+    this.mouvs.push(event.option.viewValue);
+    this.mouvInput.nativeElement.value = '';
+    this.mouvCtrl.setValue(null);
   }
 
   getErrorMessage() {
